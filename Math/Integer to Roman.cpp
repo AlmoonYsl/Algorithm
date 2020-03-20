7b2e8ae16d12a127c8f856439d86bc6a2e861cfa
Administrator <784748497@qq.com>
2020-03-20 23:27:37 +0800

    3-20

---
+++ Math/Integer to Roman.cpp
@@ -0,0 +1,18 @@
+/**
+ * leetcode no.12
+ * 
+ */
+
+string intToRoman(int num) {
+    int values[]={1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string a[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    string res;
+    for(int i = 0; i < 13; i++){
+        while(num >= values[i]){
+            num -= values[i];
+            res += a[i];
+        }
+    }
+    return res;
+}
