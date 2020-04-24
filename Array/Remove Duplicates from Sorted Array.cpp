6f9b6f5ec57a17a763de35af9f89a31d11d7fb5e
Administrator <784748497@qq.com>
2020-04-24 23:59:31 +0800

    4-24

--- Array/Remove Duplicates from Sorted Array.cpp
+++ Array/Remove Duplicates from Sorted Array.cpp
@@ -4,7 +4,7 @@
 int removeDuplicates(vector<int>& nums) {
     int len = nums.size();
     if (len < 2) return len;
-    int j = 0;
+    int j = 0; 
     for (int i = 1; i < len; i++)
         if (nums[j] != nums[i]) nums[++j] = nums[i];
     return ++j;
