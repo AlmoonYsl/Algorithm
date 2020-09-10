66da248dcb6dd149b7798ffd74af36ea743af8f5
Administrator <784748497@qq.com>
2020-09-10 11:42:19 +0800

    9-10

---
+++ Recursion/K-th Symbol in Grammar.cpp
@@ -0,0 +1,9 @@
+/**
+ * leetcode no.779
+ */
+ 
+int kthGrammar(int N, int K) {
+    if (N == 1) return 0;
+    if (K <= 1 << N - 2) return kthGrammar(N - 1, K);
+    return kthGrammar(N - 1, K - (1 << N - 2)) ^ 1;
+}
